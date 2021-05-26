f1b791833014fd8a7084fdd75a7cdebc9831d622
Sanghun <po4865+github_sanghun@gmail.com>
2021-05-26 16:43:44 +0900

    connected outlets on main storyboard - added IBOutlets in ViewController

--- AudioPlayer/ViewController.swift
+++ AudioPlayer/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
