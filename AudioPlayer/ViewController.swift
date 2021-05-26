d097d611f9686a65e35e0bf9d9ed829650a94daa
Sanghun <po4865+github_sanghun@gmail.com>
2021-05-26 17:09:59 +0900

    added actions in main storyboard - added actions for button and slider, just outline

--- AudioPlayer/ViewController.swift
+++ AudioPlayer/ViewController.swift
@@ -18,7 +18,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        
+        print("button tapped")
+        
+    }
 
-
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("slider value changed")
+    }
+    
 }
 
